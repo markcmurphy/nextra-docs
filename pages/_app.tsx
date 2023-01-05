afb374ad3f6e8bb819c1e607c4c1b1dacd068498
Mark <mark@murphymark.me>
2023-01-05 17:24:41 -0600

    first save

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,30 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import type { AppProps } from "next/app";
-import type { Page } from "types/page";
+import type { AppProps } from 'next/app';
+import type { Page } from 'types/page';
 
-import "nextra-theme-docs/style.css";
-import "@styles/global.scss";
-import "@styles/tailwind.scss";
+import 'nextra-theme-docs/style.css';
+import '@styles/global.scss';
+import '@styles/tailwind.scss';
+
+import { SessionProvider } from 'next-auth/react';
+import type { Session } from 'next-auth';
 
 type Props = AppProps & {
   Component: Page;
+  session: Session;
 };
 
-export default function Nextra({ Component, pageProps }: Props) {
+export default function Nextra({
+  Component,
+  pageProps: { session, ...pageProps },
+}: Props) {
   const getLayout = Component.getLayout ?? ((page) => page);
   const Layout = Component.layout ?? Fragment;
 
-  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
+  return (
+    <SessionProvider session={session}>
+      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
+    </SessionProvider>
+  );
 }
