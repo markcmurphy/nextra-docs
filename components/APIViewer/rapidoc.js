b278a895788ceb6d0dc2d33088b1df8147d5cde4
Mark <mark@murphymark.me>
2023-01-08 00:07:26 -0600

    deploy runner

--- components/APIViewer/rapidoc.js
+++ components/APIViewer/rapidoc.js
@@ -1,12 +1,12 @@
-import { useTheme } from "nextra-theme-docs";
-import { useEffect, useState } from "react";
+import { useTheme } from 'nextra-theme-docs';
+import { useEffect, useState } from 'react';
 
-const RapidocViewer = ({ renderStyle = "view" }) => {
+const RapidocViewer = ({ renderStyle = 'view' }) => {
   const [componentLoaded, setComponentLoaded] = useState(false);
   const { resolvedTheme } = useTheme();
 
   useEffect(() => {
-    import("rapidoc").then(() => {
+    import('rapidoc').then(() => {
       setComponentLoaded(true);
     });
   }, []);
@@ -19,13 +19,31 @@ const RapidocViewer = ({ renderStyle = "view" }) => {
         font-size="large"
         class="bg-white"
         show-header="false"
-        allow-search="false"
-        allow-advanced-search="false"
+        // server-url="/api/try-it/"
+        // default-api-server="/api/try-it/"
+        api-key-name="X-Auth-Token"
+        api-key-location="header"
+        api-key-value="r7752l82dg66kadytd52p2t06rd42k5"
+        fetch-credentials="include"
+        id="rapidoc"
+        // server-url="/api/try-it"
+        // allow-search="false"
+        // allow-advanced-search="false"
         theme={resolvedTheme}
+        onLoaded={() => {
+          console.log('loaded');
+          const rapidocEl = document.getElementById('rapidoc');
+          rapidocEl.addEventListener('before-try', (e) => {
+            console.log('try');
+            if (e.detail.request.method === 'POST') {
+              e.detail.request.headers.append('custom-token', 'AAA.BBB.CCC');
+            }
+          });
+        }}
         // onLoaded={() => {
-        //   const hash = window.location.hash
+        //   const hash = window.location.hash;
         //   if (hash !== '') {
-        //     scroller.scrollTo(hash.slice(1), { offset: -100 })
+        //     scroller.scrollTo(hash.slice(1), { offset: -100 });
         //   }
         // }}
         suppressHydrationWarning
