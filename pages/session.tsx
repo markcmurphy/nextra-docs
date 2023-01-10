570a3262363812b443ce7471ea55b645edc2d167
Mark <mark@murphymark.me>
2023-01-09 23:11:25 -0600

    session bar

---
+++ pages/session.tsx
@@ -0,0 +1,59 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+import styles from '../components/Navbar/navbar.module.css';
+
+export default function Page() {
+  const { data: session, status } = useSession();
+  const loading = status === 'loading';
+  return (
+
+      <div className={styles.signedInStatus}>
+        <p
+          className={`nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
+          {!session && (
+            <>
+              <span>You are not signed in</span>
+              <button className={`${styles.button} ${styles.buttonPrimary}`}>
+                <a
+                  href={`/api/auth/signin`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signIn();
+                  }}
+                >
+                  Sign in
+                </a>
+              </button>
+            </>
+          )}
+          {session?.user && (
+            <>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email ?? session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign out
+              </a>
+            </>
+          )}
+        </p>
+      </div>
+  );
+}
